68a16a17542445125bc00118f06fd8bfe8975f94
Pinal <pinalj1612@gmail.com>
2017-04-13 16:52:56 +0530

    Version 2.0
    Updated the version number to 2.0

--- bkap-tour-operators/plugin-updates/EDD_TOUR_BOOK_Plugin_Updater.php
+++ bkap-tour-operators/plugin-updates/EDD_TOUR_BOOK_Plugin_Updater.php
@@ -7,7 +7,7 @@
  * Allows plugins to use their own update API.
  *
  * @author Ashok Rane
- * @version 1.9
+ * @version 2.0
  */
 class EDD_TOUR_BOOK_Plugin_Updater {
 	private $api_url  = 'http://www.tychesoftwares.com/';
