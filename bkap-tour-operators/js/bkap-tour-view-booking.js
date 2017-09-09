1396ca3c96d1894322ed6b40f2fe414bbcb618e1
Dhruvin Shah <dhruvin@tychesoftwares.com>
2017-09-09 13:00:12 +0530

    JS changes to show Create Booking for admin and page slug changed to operator_bookings
    JS changes to show Create Booking for user with manage woocommerce capability an slug changed to operator_booking

--- bkap-tour-operators/js/bkap-tour-view-booking.js
+++ bkap-tour-operators/js/bkap-tour-view-booking.js
@@ -2,9 +2,9 @@ jQuery( function ( $ ) {
 
 	// Add buttons to product screen.
 	var $product_screen = $( '.edit-php.post-type-bkap_booking' ),
-		$create_action  = $product_screen.find( '.page-title-action:first' ),
-		$bkap_add_order = $( '#bkap_add_order' );
+		$create_action  = $product_screen.find( '.page-title-action:first' );
+		//$bkap_add_order = $( '#bkap_add_order' );
 
 	$create_action.hide();
-	$bkap_add_order.hide();
+	//$bkap_add_order.hide();
 });
